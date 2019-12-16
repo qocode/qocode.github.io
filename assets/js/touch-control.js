50fed879d7653c28bb7060bee7403cd8bd9623aa
IgorNovozhilov <maildrakon@gmail.com>
2019-12-16 23:36:32 +0300

    fix touch

--- assets/js/touch-control.js
+++ assets/js/touch-control.js
@@ -1,11 +1,12 @@
 const { screen, document } = window
 let touchStartX = 0
+let touchEndX = 0
 const qrscanner = document.querySelector('qrcode-scanner')
 const mainmenu = document.querySelector('main-menu')
 
 
 document.body.addEventListener('touchstart', function touchstart(e) {
-  touchStartX = e.targetTouches[0].clientX
+  touchEndX = touchStartX = e.targetTouches[0].clientX
 }, false)
 
 document.body.addEventListener('touchmove', function touchmove(e) {
@@ -14,25 +15,29 @@ document.body.addEventListener('touchmove', function touchmove(e) {
       if (qrscanner.hasAttribute('opened')) {
         qrscanner.toggleScan()
       } else {
-        mainmenu.setAttribute('opened', '')
+        if (touchStartX < screen.width / 10) {
+          mainmenu.setAttribute('opened', '')
+        }
       }
       touchStartX = 0
     } else if (touchStartX - e.targetTouches[0].clientX > screen.width / 10) {
       if (mainmenu.hasAttribute('opened')) {
         mainmenu.removeAttribute('opened')
       } else {
-        if (!qrscanner.hasAttribute('opened')) {
+        if (!qrscanner.hasAttribute('opened') && touchStartX > (screen.width - screen.width / 10)) {
           qrscanner.toggleScan()
-        } else {
-          // TODO: переделать на клик
-          qrscanner.append()
         }
       }
       touchStartX = 0
     }
   }
+  touchEndX = e.targetTouches[0].clientX
 }, false)
 
 document.body.addEventListener('touchend', function touchend(e) {
+  if (touchStartX > 0 && Math.abs(touchStartX - touchEndX) < screen.width / 10 &&
+    qrscanner.hasAttribute('opened')) {
+    qrscanner.append()
+  }
   touchStartX = 0
 }, false)
